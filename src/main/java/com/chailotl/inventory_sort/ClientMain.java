7e1579c2f4c8a225681e9f85a1dca6adcd8f02e2
Chai <7232280+Chailotl@users.noreply.github.com>
2024-05-03 15:35:30 -0400

    Use dynamic strings to identify screen handler classes

--- src/main/java/com/chailotl/inventory_sort/ClientMain.java
+++ src/main/java/com/chailotl/inventory_sort/ClientMain.java
@@ -7,7 +7,6 @@ import com.chailotl.inventory_sort.buttons.SortInventoryButton;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
-import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.screen.*;
 
@@ -16,26 +15,26 @@ import java.util.Hashtable;
 
 public class ClientMain implements ClientModInitializer
 {
-	private final HashSet<Class<? extends ScreenHandler>> ignoreScreens = new HashSet<>();
-	private final HashSet<Class<? extends ScreenHandler>> containerScreens = new HashSet<>();
-	private final Hashtable<Class<? extends ScreenHandler>, Integer> offsetScreens = new Hashtable<>();
+	private final HashSet<String> ignoreScreens = new HashSet<>();
+	private final HashSet<String> containerScreens = new HashSet<>();
+	private final Hashtable<String, Integer> offsetScreens = new Hashtable<>();
 
 	@Override
 	public void onInitializeClient()
 	{
-		ignoreScreens.add(BeaconScreenHandler.class);
-		ignoreScreens.add(LoomScreenHandler.class);
-		ignoreScreens.add(CreativeInventoryScreen.CreativeScreenHandler.class);
+		ignoreScreens.add("net.minecraft.screen.BeaconScreenHandler");
+		ignoreScreens.add("net.minecraft.screen.LoomScreenHandler");
+		ignoreScreens.add("net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen.CreativeScreenHandler");
 
-		containerScreens.add(GenericContainerScreenHandler.class);
-		containerScreens.add(ShulkerBoxScreenHandler.class);
-		containerScreens.add(Generic3x3ContainerScreenHandler.class);
-		containerScreens.add(HopperScreenHandler.class);
-		containerScreens.add(HorseScreenHandler.class);
+		containerScreens.add("net.minecraft.screen.GenericContainerScreenHandler");
+		containerScreens.add("net.minecraft.screen.ShulkerBoxScreenHandler");
+		containerScreens.add("net.minecraft.screen.Generic3x3ContainerScreenHandler");
+		containerScreens.add("net.minecraft.screen.HopperScreenHandler");
+		containerScreens.add("net.minecraft.screen.HorseScreenHandler");
 
-		offsetScreens.put(GenericContainerScreenHandler.class, -1);
-		offsetScreens.put(ShulkerBoxScreenHandler.class, -1);
-		offsetScreens.put(EnchantmentScreenHandler.class, 1);
+		offsetScreens.put("net.minecraft.screen.GenericContainerScreenHandler", -1);
+		offsetScreens.put("net.minecraft.screen.ShulkerBoxScreenHandler", -1);
+		offsetScreens.put("net.minecraft.screen.EnchantmentScreenHandler", 1);
 
 		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) ->
 		{
@@ -43,18 +42,38 @@ public class ClientMain implements ClientModInitializer
 
 			ScreenHandler screenHandler = handledScreen.getScreenHandler();
 
-			if (ignoreScreens.stream().anyMatch(clazz -> clazz.isInstance(screenHandler))) { return; }
+			//if (ignoreScreens.stream().anyMatch(clazz -> clazz.isInstance(screenHandler))) { return; }
+			if (ignoreScreens.stream().anyMatch(clazz -> {
+				try
+				{
+					return Class.forName(clazz).isInstance(screenHandler);
+				}
+				catch (ClassNotFoundException e)
+				{
+					return false;
+				}
+			})) { return; }
 
 			// Need to figure out a way to update it when changing tabs
 			//if (handledScreen instanceof CreativeInventoryScreen && ((AccessorCreativeInventoryScreen) handledScreen).getSelectedTab().getType() != ItemGroup.Type.INVENTORY) { return; }
 
-			int y = offsetScreens.getOrDefault(screenHandler.getClass(), 0);
+			//int y = offsetScreens.getOrDefault(screenHandler.getClass(), 0);
+			int y = offsetScreens.getOrDefault(screenHandler.getClass().getCanonicalName(), 0);
 
 			// Adding buttons
 			SortInventoryButton sortInventoryButton = new SortInventoryButton(handledScreen, 0, y);
 
-			if (containerScreens.stream().anyMatch(clazz -> clazz.isInstance(screenHandler)) ||
-				screenHandler.slots.size() >= 36 + 18)
+			//if (containerScreens.stream().anyMatch(clazz -> clazz.isInstance(screenHandler)) ||
+			if (containerScreens.stream().anyMatch(clazz -> {
+				try
+				{
+					return Class.forName(clazz).isInstance(screenHandler);
+				}
+				catch (ClassNotFoundException e)
+				{
+					return false;
+				}
+			}) || screenHandler.slots.size() >= 36 + 18)
 			{
 				DepositButton depositButton = new DepositButton(handledScreen, -11, y);
 				LootButton lootButton = new LootButton(handledScreen, -22, y);
